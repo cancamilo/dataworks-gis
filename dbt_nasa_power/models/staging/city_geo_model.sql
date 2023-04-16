602dffe47a7aec81564e028ab636c82af2c4b294
Camilo Ramirez <camiloramirezf@hotmail.com>
2023-04-16 16:36:25 +0000

    add flux and geo models

--- dbt_nasa_power/models/staging/city_geo_model.sql
+++ dbt_nasa_power/models/staging/city_geo_model.sql
@@ -1,32 +1,28 @@
-WITH city_geo AS (
+WITH cities_data AS (
   SELECT
-      id,
-      city,
+      id AS city_id,
+      city AS city_name,
+      city_ascii,
       country,
-      capital,
       population,
-      admin_name,      
       ST_GEOGPOINT(lng, lat) AS city_coords
-    FROM `dataworks-gis.test_gis.cities`
+    FROM {{ source('staging','cities') }}
+    -- dbt build --m <model.sql> --var 'is_test_run: false'
+    {% if var('is_test_run', default=true) %} 
+    limit 100 -- use only a few cities for testing
+    {% endif %}
 ), metrics_geo AS (
   SELECT
-    GEO_ID,
+    {{ dbt_utils.surrogate_key(['lon', 'lat', 'time']) }} AS GEO_ID,
     time,
-    T2M,
-    T10M,
+    T2M - 273.15,
+    T10M - 273.15,
     WS2M,
     WS10M,
     PRECTOTCORR,
     ST_GEOGPOINT(lon, lat) AS metric_coords
-  FROM `dataworks-gis.test_gis.ceres`
+  FROM {{ source('staging','geos_table_partitioned') }}
 )
-SELECT * FROM city_geo a
+SELECT * FROM cities_data a
 JOIN metrics_geo b
 ON ST_DWithin(a.city_coords, b.metric_coords, 50000)
-
--- dbt build --m <model.sql> --var 'is_test_run: false'
-{% if var('is_test_run', default=true) %}
-
-  limit 100
-
-{% endif %}
