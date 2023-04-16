d58f6b869e79b43ef1a83cbb9f418717fd6a5dd8
Camilo Ramirez <camiloramirezf@hotmail.com>
2023-04-16 17:33:00 +0000

    add basic test to id columns

--- dbt_nasa_power/models/core/city_monthly_agg.sql
+++ dbt_nasa_power/models/core/city_monthly_agg.sql
@@ -1,12 +1,12 @@
 SELECT 
-  city_name, 
-  FORMAT_DATETIME("%B", DATETIME(time)),
-  AVG(T2M_CELCIUS) AS monthly_average_temperature,
-  AVG(WS2M) AS monthly_average_windspeed,
-  AVG(PRECTOTCORR) AS monthly_average_precipitations,
-  AVG(ALLSKY_SFC_LW_DWN) As mothly_average_termal_irradiance_lw,
-  AVG(ALLSKY_SFC_SW_DWN) As mothly_average_termal_irradiance_sw
-FROM {{ref('city_flux_model')}} a
-INNER JOIN {{ref('city_geos_model')}} b
-ON a.city_id = b.city_id
-GROUP BY city, FORMAT_DATETIME("%B", DATETIME(time))
+  a.city_name, 
+  FORMAT_DATETIME("%B", DATETIME(a.time)) as month_name,
+  AVG(a.T2M_CELCIUS) AS monthly_average_temperature,
+  AVG(a.WS2M) AS monthly_average_windspeed,
+  AVG(a.PRECTOTCORR) AS monthly_average_precipitations,
+  AVG(b.ALLSKY_SFC_LW_DWN) As mothly_average_termal_irradiance_lw,
+  AVG(b.ALLSKY_SFC_SW_DWN) As mothly_average_termal_irradiance_sw
+FROM {{ref('city_geos_model')}} a
+INNER JOIN {{ref('city_flux_model')}} b
+ON a.city_id = b.city_id AND a.time = b.time
+GROUP BY a.city_name, FORMAT_DATETIME("%B", DATETIME(a.time))
